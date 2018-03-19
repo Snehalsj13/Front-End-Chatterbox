1bdb846b08636c0d1875e1ecf8d3615fe5140823
snehalsj13 <snehal.snehal4@gmail.com>
2018-03-19 14:34:09 -0700

    Initial commit

---
+++ chatterbox/websockets-server.js
@@ -0,0 +1,24 @@
+/* global require */
+var WebSocket = require("ws");
+var WebSocketServer = WebSocket.Server;
+var port = 3001;
+var ws = new WebSocketServer({
+  port: port
+});
+var messages = [];
+/*eslint no-console: ["error", { allow: ["warn", "error","log"] }] */
+console.log("websockets server started");
+
+ws.on("connection", function(socket) {
+  console.log("client connection established");
+  messages.forEach(function(msg) {
+    socket.send(msg);
+  });
+  socket.on("message", function(data) {
+    console.log("message received: " + data);
+    messages.push(data);
+    ws.clients.forEach(function(clientSocket) {
+      clientSocket.send(data)
+    });
+  });
+});
